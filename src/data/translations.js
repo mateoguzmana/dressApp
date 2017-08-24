2915974aeb58826dad2f30eaeef5505d3619c13c
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2017-08-24 08:19:40 -0500

    Added translations file

---
+++ src/data/translations.js
@@ -0,0 +1,16 @@
+export default Translations = {
+    clothesView: {
+        title: {
+            en: "Clothes",
+            sp: "Ropa",
+            pt: "Roupas",
+            fr: "Vêtements"
+        },
+        description: {
+            en: "Clothes for ${gender}",
+            sp: "Ropa para ${gender}",
+            pt: "Roupas pra ${gender}",
+            fr: "Vêtements pour ${gender}"
+        }
+    }
+}
